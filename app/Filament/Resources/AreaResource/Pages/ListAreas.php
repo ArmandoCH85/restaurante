ebd860fe2cee255a2b7a2600d4d4daaa1f48c56c
ArmandoCH85 <armando.correah@gmail.com>
2026-02-16 22:19:36 -0500

    update area

--- app/Filament/Resources/AreaResource/Pages/ListAreas.php
+++ app/Filament/Resources/AreaResource/Pages/ListAreas.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\AreaResource\Pages;
 
 use App\Filament\Resources\AreaResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAreas extends ListRecords
 {
     protected static string $resource = AreaResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Crear area'),
+        ];
+    }
 }
