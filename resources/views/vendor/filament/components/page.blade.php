d9f66aad0c1840a80de2a395dedaa3908a7d96a9
ArmandoCH85 <armando.correah@gmail.com>
2026-02-20 18:25:50 -0500

    feat: sidebar and page layout improvements

--- resources/views/vendor/filament/components/page.blade.php
+++ resources/views/vendor/filament/components/page.blade.php
@@ -37,4 +37,6 @@
     @endif
 </div>
 
-<script src="{{ asset('js/cash-register-modal.js') }}"></script>
+@if (file_exists(public_path('js/cash-register-modal.js')))
+    <script src="{{ asset('js/cash-register-modal.js') }}"></script>
+@endif
